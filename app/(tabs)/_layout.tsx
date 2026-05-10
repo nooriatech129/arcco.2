6ed791ef80fbf8aa82670793518861ebbd57083a
Nooria Sediqi <nsediqi8770@sdsu.edu>
2026-05-09 18:16:22 -0700

    Add occupancy navigation tab (R3 mitigation)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,4 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
@@ -30,6 +29,7 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen name="occupancy" options={{ title: "Occupancy" }} />
     </Tabs>
   );
 }
